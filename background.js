ee4f7c25c6b4c3c6994954cb7159b65eec40e987
daimatz <dai@daimatz.net>
2015-09-27 17:13:03 +0900

    create Real Time Messaging session

--- background.js
+++ background.js
@@ -3,16 +3,50 @@ var menus = [];
 var App = function(token) {
   this.token = token;
 }
-App.prototype.run = function() {
-  var notification = chrome.notifications.create(
+App.prototype.postNotification = function(message, url) {
+  return chrome.notifications.create(
     null,
     {
       type: "basic",
-      title: "Hello!",
-      message: "this is an example.",
+      title: this.token,
+      message: message,
       iconUrl: "icon.png"
+    },
+    function(createdId) {
+      chrome.notifications.onClicked.addListener(function(clickedId) {
+        if (createdId === clickedId) {
+          window.open(url);
+        }
+      });
     }
   );
+};
+App.prototype.createRealtime = function(callback) {
+  var xhr = new XMLHttpRequest();
+  var url = "https://slack.com/api/rtm.start?token=" + this.token;
+  xhr.open("GET", url, true);
+  xhr.onload = function(e) {
+    if (xhr.readyState == 4) {
+      if (xhr.status === 200) {
+        var res = xhr.responseText;
+        var json = JSON.parse(res);
+        var ws = new WebSocket(json.url);
+        if (callback) {
+          callback(ws);
+        }
+      } else {
+      }
+    }
+  };
+  xhr.send();
+};
+App.prototype.run = function() {
+  this.createRealtime(function(ws) {
+    this.postNotification(
+        "WebSocket URL is: " + ws.url,
+        "http://www.google.com"
+    );
+  }.bind(this));
 };
 
 var reload = function(info, tab) {
